fa8dddb33bce5b25176e70aa7d57727bd619a347
Brandon Powell <brandon.powell@armory.io>
2020-01-03 14:41:30 -0800

    feat(frontend): uses new OSS deck changes for plugins

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -1,45 +1,35 @@
-import builtins from 'rollup-plugin-node-builtins';
-import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import replace from 'rollup-plugin-replace'
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
+import postCss from 'rollup-plugin-postcss';
+import externalGlobals from 'rollup-plugin-external-globals';
 
-import pkg from './package.json'
+export default [
+  {
+    input: 'src/index.tsx',
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript(),
+      // map imports from shared libraries (react, etc) to global variables exposed by spinnaker
+      externalGlobals(spinnakerSharedLibraries()),
+      // import from .css, .less, and inject into the document <head></head>
+      postCss(),
+    ],
+    output: [{ dir: 'dist', format: 'es', }]
+  }
+];
 
-export default {
-  input: 'src/index.tsx',
-  output: [
-    {
-      file: pkg.main,
-      format: 'iife',
-      exports: 'named',
-      sourcemap: true,
-      name: "MyStage"
-    },
-  ],
-  plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify( 'production' )
-    }),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      clean: true
-    }),
-    commonjs(
-      {
-        include: ["./src/index.tsx", "node_modules/**",],
-        namedExports: {
-          'node_modules/react/index.js': ['Children', 'Component', 'PropTypes', 'createElement'],
-        }
-      }
-    ),
-    resolve(
-      {
-        jsnext: true,
-        main: true,
-        modules: true
-      }
-    ),
-    builtins(),
-  ]
+function spinnakerSharedLibraries() {
+  const libraries = ['lodash', 'react', 'react-dom', '@spinnaker/core'];
+
+  function getGlobalVariable(libraryName) {
+    const prefix = 'spinnaker.plugins.sharedLibraries';
+    const sanitizedLibraryName = libraryName.replace(/[^a-zA-Z0-9_]/g, '_');
+    return `${prefix}.${sanitizedLibraryName}`;
+  }
+
+  return libraries.reduce((globalsMap, libraryName) => {
+    return { ...globalsMap, [ libraryName ]: getGlobalVariable(libraryName) }
+  }, {});
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,46 +1,35 @@
 import * as React from 'react';
-// IPluginInitialize is function interface
-// that takes in the IStageRegistry interface.
-// The IStageRegistry is used to register the stage.
-import { IPluginInitialize, IStageRegistry } from '@spinnaker/plugins';
+import { IStageTypeConfig, IStageConfigProps } from '@spinnaker/core';
 
-// Our stage component
-class RandomWaitStage extends React.Component {
-  setMaxWaitTime = (event: React.SyntheticEvent) => {
-    let target = event.target as HTMLInputElement;
-    // @ts-ignore
-    this.props.updateStageField({'maxWaitTime': target.value});
-  }
+const customStage: IStageTypeConfig = {
+  label: 'Random Wait',
+  description: 'Stage that waits a random amount of time up to the max inputted',
+  key: 'randomWait',
+  component: RandomWaitStage,
+};
 
-  render() {
-    return (
-      <div>
-        <label>
-            Max Time To Wait
-            <input onChange={this.setMaxWaitTime} id="maxWaitTime" />
-        </label>
-      </div>
-    );
-  }
+function setMaxWaitTime(event: React.SyntheticEvent, props: IStageConfigProps) {
+  let target = event.target as HTMLInputElement;
+  // @ts-ignore
+  props.updateStageField({'maxWaitTime': target.value});
 }
 
-// This function implements the IPluginInitialize interface
-// This is where the stage gets registered.
-function initialize(registry: IStageRegistry): void {
-  registry.pipeline.registerStage({
-    label: 'Random Wait',
-    description: 'Stage that waits a random amount of time up to the max inputted',
-    key: 'randomWait',
-    component: RandomWaitStage,
-  });
-};
+// Our stage component
+function RandomWaitStage(props: IStageConfigProps) {
+  console.error(props);
+  return (
+    <div>
+      <label>
+          Max Time To Wait
+          <input onChange={(e) => setMaxWaitTime(e, props)} id="maxWaitTime" />
+      </label>
+    </div>
+  );
+}
 
-// Make the initialize function be the interface
-let init: IPluginInitialize = initialize;
 const plugin = {
-  initialize: init,
+  name: 'randomWait',
+  stages: [customStage],
 };
 
-// Call spinnaker settings to actually load the stage
-// plugin for us
-window.spinnakerSettings.onPluginLoaded(plugin);
+export { plugin };
